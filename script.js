a19cae33e20d0eecd59d98ea375bb433515e02b4
Ygor de Oiveira Souza da Silva <ygor90928@gmail.com>
2025-04-16 16:00:57 -0300

    File of project

---
+++ script.js
@@ -0,0 +1,11 @@
+function Converter() {
+    let valorElemento = document.getElementById("valor");
+    let valor = valorElemento.value;
+    let valorEmDolarNumerico = parseFloat(valor);
+
+    let valorEmReal = valorEmDolarNumerico * 5;
+
+    let elementoValorConvertido = document.getElementById("valorConvertido");
+    let valorConvertido = "O resultado em real é R$" + valorEmReal;
+    elementoValorConvertido.innerHTML = valorConvertido;
+}
